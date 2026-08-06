7f409b3083ae5851321680281e07d0effe1bd4b1
slothgina <gina.slothstudio@zohomail.com>
2026-08-05 20:05:41 -0400

    imported react hooks

--- WebApiLab.Web/src/App.jsx
+++ WebApiLab.Web/src/App.jsx
@@ -1,11 +1,49 @@
 import './App.css'
+import React, {useState, useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:5173/api/People');
+
+        if (!response.ok)
+        {
+          throw new Error('HTTP error! status: ${response.status}');
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }  
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>
+  } else if (error) {
+    return <div>Error: {error}</div>
+  } else if (data) {
+    return (
+      <div>
+        <h1>Fetched Data:</h1>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    )
+  }
 
   return (
     <span>Hello,World!</span>
-  )
+  );
 }
 
 export default App
